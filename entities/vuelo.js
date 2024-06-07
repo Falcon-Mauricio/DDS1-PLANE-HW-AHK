6093c9aae5f32859451af5ea2a46e8c73a578caa
Mauricio Falcon <105568570+Falcon-Mauricio@users.noreply.github.com>
2024-06-07 10:22:20 -0300

    Creacion del test vuelo

--- entities/vuelo.js
+++ entities/vuelo.js
@@ -1,8 +1,14 @@
 export class Vuelo {
-    pasajeros;
-    empleados;
+    pasajeros = [];
+    empleados = [];
     avion;
     fechaHoraPartida;
     fechaHoraSalida;
     aeropuesrtoLlegada;
+    agregarPasajero(pasajero){
+        this.pasajeros.push(pasajero);
+    }
+    capacidadOcupadaPorPasajeros(){
+        return (this.pasajeros.length * 100) / this.avion.cantidadAsientos;
+    }
 }
---
+++ test/vuelo.test.js
@@ -0,0 +1,26 @@
+import { Vuelo } from "../entities/vuelo";
+import { Avion } from "../entities/avion";
+import { Pasajero } from "../entities/pasajero";
+
+let pasajeroFeliz1;
+let pasajeroFeliz2;
+let avionFeliz;
+
+beforeEach(() => {
+    pasajeroFeliz1 = new Pasajero()
+    pasajeroFeliz2 = new Pasajero()
+    avionFeliz = new Avion();
+    avionFeliz.cantidadAsientos = 4;
+})
+
+test("La capacidad de pasajeros es de 50% (avion con 4 asientos)", () => {
+    const vueloFeliz = new Vuelo();
+    vueloFeliz.avion = avionFeliz;
+    vueloFeliz.agregarPasajero(pasajeroFeliz1);
+    vueloFeliz.agregarPasajero(pasajeroFeliz2);
+    
+    const promedio = vueloFeliz.capacidadOcupadaPorPasajeros();
+    const promedioEsperado = 50;
+
+    expect(promedio).toBe(promedioEsperado);
+});
