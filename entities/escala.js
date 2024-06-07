a4262c73378a6d3f522420d36eec1a808da96b42
Mauricio Falcon <105568570+Falcon-Mauricio@users.noreply.github.com>
2024-06-07 09:07:38 -0300

    Cracion del readme

--- entities/escala.js
+++ entities/escala.js
@@ -2,4 +2,8 @@ import { Aeropuerto } from "./aeropuerto";
 export class Escala {
     aeropuerto;
     duracionAproxEnMin;
+    constructor(aeropuerto, duracionAproxEnMin){
+        this.aeropuerto = aeropuerto;
+        this.duracionAproxEnMin = duracionAproxEnMin;
+    }
 }
